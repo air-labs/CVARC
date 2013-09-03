550563fdc2f4c6abed5f411a7dc42141b3f632ff
okulovsky <yuri.okulovsky@gmail.com>
2013-09-03 16:44:23 +0600

    Сменил CVARK.Network -> CVARC.Network

---
+++ Competitions/LocalNetworkClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNetworkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Process();
+            p.StartInfo.FileName = "..\\..\\..\\..\\CVARC\\CVARC.Network\\bin\\Debug\\CVARC.Network.exe";
+            p.StartInfo.Arguments= "..\\..\\..\\..\\Competitions\\Fall2013.0\\bin\\Debug\\Fall2013.0.dll";
+            p.Start();
+        }
+    }
+}
