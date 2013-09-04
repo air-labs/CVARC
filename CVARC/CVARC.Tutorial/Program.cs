65f774b41721ddbaf3d08c49497facbbb107bccd
okulovsky <yuri.okulovsky@gmail.com>
2013-09-04 12:11:47 +0600

    Пока ничего не работает, комит для просмотра оригинальной версии

--- CVARC/CVARC.Tutorial/Program.cs
+++ CVARC/CVARC.Tutorial/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
 using CVARC.Basic;
 using CVARC.Basic.Controllers;
+using CVARC.Core;
+using CVARC.Graphics;
+using CVARC.Physics;
 
 namespace CVARC.Tutorial
 {
     static class Program
     {
+        static Body root;
+        static TutorialForm form;
+        static Competitions competitions;
+
+        static List<Keys> pressedKeys = new List<Keys>();
+        static void MakeCycle(double time, bool realtime)
+        {
+            double dt=1.0 / 10000;
+            for (double t = 0; t < time; t += dt)
+            {
+                PhysicalManager.MakeIteration(dt, root);
+                foreach (Body body in root)
+                    body.Update(1 / 60);
+            }
+            form.BeginInvoke(new Action(form.UpdateScores));
+        }
+
+        static void form_KeyUp(object sender, KeyEventArgs e)
+        {
+            lock (pressedKeys)
+            {
+                pressedKeys.Remove(e.KeyCode);
+            }
+        }
+
+        static void form_KeyDown(object sender, KeyEventArgs e)
+        {
+            lock (pressedKeys)
+            {
+                if (!pressedKeys.Contains(e.KeyCode)) pressedKeys.Add(e.KeyCode);
+            }
+        }
+
+        static void Process()
+        {
+            while (true)
+            {
+                lock (pressedKeys)
+                {
+                    var commands = pressedKeys.SelectMany(z => competitions.KeyboardController.GetCommand(z)).ToList();
+                }
+
+
+
+            }
+        }
+
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -22,12 +75,29 @@ namespace CVARC.Tutorial
                return;
            }
 
-           var competitions = Competitions.Load(args[0]);
+           competitions = Competitions.Load(args[0]);
+           root = competitions.World.Init();
+           PhysicalManager.InitializeEngine(PhysicalEngines.Farseer, root);
+           var factory = new DrawerFactory(root);
+           competitions.Behaviour.InitSensors();
+           competitions.Behaviour.Sensors.ForEach(a =>
+           {
+               foreach (var robot in competitions.World.Robots)
+               {
+                   var sens = a.GetOne(robot, competitions.World, factory);
+                   robot.Sensors.Add(sens);
+               }
+           });
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            form = new TutorialForm(competitions.World, factory);
+            form.KeyDown += form_KeyDown;
+            form.KeyUp += form_KeyUp;
          //   Application.Run(new TutorialForm(com., b, kb));
         //    Application.Run(new TestForm());
         }
+
+       
     }
 }
--- CVARC/CVARC.Tutorial/TutorialForm.cs
+++ CVARC/CVARC.Tutorial/TutorialForm.cs
@@ -17,71 +17,31 @@ namespace CVARC.Tutorial
 
     public partial class TutorialForm : Form
     {
-        private readonly RobotBehaviour _robotBehaviour;
-        public World World { get; set; }
         private const AnchorStyles AnchorAll = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-        protected override void OnLoad(EventArgs e)
+        public ScoreDisplayControl ScoreDisplayControl;
+        public PictureBox BitmapDisplayer;
+
+        public void UpdateScores()
         {
-            base.OnLoad(e);
-            PhysicalManager.InitializeEngine(PhysicalEngines.Farseer, Root);
-            var factory = new DrawerFactory(Root);
-            _robotBehaviour.InitSensors();
-            _robotBehaviour.Sensors.ForEach(a =>
-                {
-                    foreach(var robot in World.Robots)
-                    {
-                        var sens = a.GetOne(robot, World, factory);
-                        robot.Sensors.Add(sens);
-                    }
-                });
+            ScoreDisplayControl.UpdateDisplayedScores();
+        }
+
+        public TutorialForm(World world, DrawerFactory factory)
+        {
+            InitializeComponent();
+            ClientSize = new Size(800, 600);
+            ScoreDisplayControl = new ScoreDisplayControl{Scores = world.Score};
             tableLayoutPanel1.Controls.Add(new DrawerControl(new DirectXFormDrawer(factory.GetDirectXScene(), new DrawerSettings { ViewMode = ViewModes.Top })), 1, 0);
-            for (int i = 0; i < World.RobotCount; ++i )
+            for (int i = 0; i < world.RobotCount; ++i)
                 tableLayoutPanel1.Controls.Add(new DrawerControl(new DirectXFormDrawer(factory.GetDirectXScene(), new DrawerSettings
                 {
                     BodyCameraLocation = new Frame3D(30, 0, 30, Angle.FromGrad(-45), Angle.Zero, Angle.Zero),
-                    Robot = World.Robots[i].Body,
+                    Robot = world.Robots[i].Body,
                     ViewMode = ViewModes.FirstPerson
                 })), 0, 0);
             tableLayoutPanel1.Controls.Add(ScoreDisplayControl, 1, 1);
             foreach (Control control in tableLayoutPanel1.Controls)
                 control.Anchor = AnchorAll;
-            new Thread(() =>
-            {
-                while (true)
-                    MakeCycle(true);
-            })
-            {
-                IsBackground = true
-            }.Start();
-        }
-
-        protected Body Root { get; set; }
-
-        protected virtual void MakeCycle(bool realtime)
-        {
-            PhysicalManager.MakeIteration(1.0/10000, Root);
-            foreach (Body body in Root)
-                body.Update(1/60);
-            ScoreDisplayControl.UpdateDisplayedScores();
-        }
-
-        public ScoreDisplayControl ScoreDisplayControl;
-        public PictureBox BitmapDisplayer;
-
-        public TutorialForm(World world, RobotBehaviour robotBehaviour, Type kb)
-        {
-            _robotBehaviour = robotBehaviour;
-            World = world;
-            InitializeComponent();
-            ClientSize = new Size(800, 600);
-            Root = world.Init();
-            ScoreDisplayControl = new ScoreDisplayControl{Scores = world.Score};
-            var kbContr = kb.GetConstructor(new[] {typeof(Form)});
-            if(kbContr != null)
-            {
-                var contr = kbContr.Invoke(new object[]{this}) as KeyboardController;
-                if(contr != null) contr.ProcessCommand += command => robotBehaviour.ProcessCommand(World.Robots[command.RobotId], command);
-            }
         }
     }
 }
