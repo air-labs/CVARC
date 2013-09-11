790c80d6d9c4e8696ebdc6d9e8748f2abe458b94
okulovsky <yuri.okulovsky@gmail.com>
2013-09-11 20:49:58 +0600

    Забыл bat-файл

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+﻿mkdir deploy
+xcopy CVARC\CVARC.Network\bin\debug\*.dll deploy\ /y
+xcopy CVARC\CVARC.Network\bin\debug\*.exe deploy\ /y
+xcopy CVARC\CVARC.Tutorial\bin\debug\*.exe deploy\ /y
+xcopy CVARC\CVARC.Tutorial\bin\debug\*.dll deploy\ /y
+xcopy Competitions\Fall2013.0\bin\Debug\*.dll deploy\ /y
+xcopy Competitions\DemoNetworkClient\bin\Debug\*.exe deploy\ /y
+"C:\Program Files\WinRar\rar" a deploy deploy\*.*
+rd deploy /s /q
