ab2243ea3489ae89d24066771ce0c226c10a9c43
okulovsky <yuri.okulovsky@gmail.com>
2014-07-05 00:17:14 +0600

    Структура каталогов приведена в соответствие с солюшном

---
+++ CVARC.Engine/Graphics/DirectX/DirectXExtensions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using AIRLab.Mathematics;
+using CVARC.Core;
+using SlimDX;
+using SlimDX.Direct3D9;
+using Matrix = SlimDX.Matrix;
+
+namespace CVARC.Graphics.DirectX
+{
+	public static class DirectXExtensions
+	{
+		public static ExtendedMaterial GetDirectXMaterial(this Color color)
+		{
+			return new ExtendedMaterial
+			       	{
+			       		MaterialD3D = new Material
+			       		              	{
+			       		              		Ambient = color,
+			       		              		Diffuse = color,
+			       		              		Specular = color,
+			       		              		Power = 15f
+			       		              	}
+			       	};
+		}
+
+		/// <summary>
+		/// Возвращает матрицу координат тела относительно родителя
+		/// (в координатной системе DirectX)
+		/// </summary>
+		/// <param name="body">Тело</param>
+		/// <returns></returns>
+		public static Matrix GetRelativeLocationMatrix(this Body body)
+		{
+			return body.Location.ToDirectXMatrix();
+		}
+
+		/// <summary>
+		/// Возвращает матрицу абсолютных координат тела
+		/// (в коорлинатной системе DirectX)
+		/// </summary>
+		/// <param name="body">Тело</param>
+		/// <returns></returns>
+		public static Matrix GetAbsoluteLocationMatrix(this Body body)
+		{
+			return body.GetAbsoluteLocation().ToDirectXMatrix();
+		}
+
+		/// <summary>
+		/// Возвращает матрицу в системе координат DirectX
+		/// </summary>
+		/// <param name="frame">Фрейм</param>
+		/// <returns></returns>
+		public static Matrix ToDirectXMatrix(this Frame3D frame)
+		{
+			//Если нет кватерниона - поворачиваем исходя из Pitch, yaw, roll - иначе через кватернион.
+			if (!frame.Orientation.HasValue)
+			{
+				return Matrix.RotationYawPitchRoll((float) frame.Pitch.Radian,
+				                                   (float) frame.Roll.Radian, -(float) frame.Yaw.Radian)*
+				       Matrix.Translation(-(float) frame.X, (float) frame.Y, (float) frame.Z);
+			}
+			Quat quat = frame.Orientation.Value;
+			var q = new Quaternion((float) quat.X, (float) quat.Y, (float) quat.Z, (float) quat.W);
+			return Matrix.RotationQuaternion(q)*
+			       Matrix.Translation(-(float) frame.X, (float) frame.Y, (float) frame.Z);
+		}
+
+		/// <summary>
+		/// Из точки возвращает вектор для DirectX
+		/// </summary>
+		/// <param name="point"></param>
+		/// <returns></returns>
+		public static Vector3 ToDirectXVector(this Point3D point)
+		{
+			return new Vector3(-(float) point.X, (float) point.Y, (float) point.Z);
+		}
+
+		///<summary>
+		/// Создает DirectX-овский объект Light из данных настроек.
+		/// </summary>
+		/// <returns></returns>
+		public static Light ToDirectXLight(this LightSettings lsets)
+		{
+			Color color = Color.FromName(lsets.ColorString);
+			var l = new Light
+			        {
+			        	Diffuse = color,
+			        	Specular = color,
+			        	Direction = lsets.Direction.ToDirectXVector(),
+						Position = lsets.Position.ToDirectXVector(),
+			        	Type = LightType.Directional,
+						Phi = (float) lsets.OuterAngle.Radian,
+						Theta = (float) lsets.InnerAngle.Radian,
+						Falloff = 1.0f,
+						Range = 1000,
+						Attenuation0 =1f,
+ 						Attenuation1 = 0.001f,
+			        };
+			if (lsets.Type == LightSettings.MyLightType.Ambient)
+			{
+				l.Ambient = color;
+				l.Direction = -Vector3.UnitZ;
+			}
+
+			LightType x;
+			if (Enum.TryParse(lsets.Type.ToString(), out x))
+				l.Type = x;
+			return l;
+		}
+
+		public static Vector3 ToDirectXVector(this Frame3D frame)
+		{
+			return new Vector3(-(float) frame.X, (float) frame.Y, (float) frame.Z);
+		}
+	}
+}
---
+++ CVARC.Engine/Graphics/SceneConfig.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using AIRLab.Mathematics;
+
+namespace CVARC.Graphics
+{
+	/// <summary>
+	/// Класс содержит редко меняющиеся глобальные настройки сцены
+	/// </summary>
+	public static class SceneConfig
+	{
+		public static readonly Angle FirstPersonViewAngle = Angle.HalfPi;
+		public static readonly Angle ThirdPersonViewAngle = Angle.FromGrad(45);
+
+		public static readonly List<LightSettings> Lights = new List<LightSettings>
+		{
+			new LightSettings
+			{
+               ColorString = "White",
+               Direction = new Point3D(0,0, -1),
+               Type = LightSettings.MyLightType.Directional
+			},
+            new LightSettings
+			{
+               ColorString = "White",
+               Type = LightSettings.MyLightType.Ambient
+			}
+		};
+
+		public const int VideoHeight = 600;
+		public const int VideoWidth = 800;
+		public static int Framerate = 50;
+		public const bool EnableShadows = false;
+		public const bool EnableSpecularHighlights = false;
+
+		public const float NearClipDistance = 1;
+		public const float FarClipDistance = 800;
+	}
+
+	/// <summary>
+	/// Настройки для источника света. Нужны тк Light в WPF и Light в DirectX-разные объекты
+	/// </summary>
+	public class LightSettings
+	{
+		public MyLightType Type;
+		public Point3D Position;
+		public Point3D Direction;
+		public String ColorString;
+		/// <summary>
+		/// Внутренний угол для Type=Spot
+		/// </summary>
+		public Angle InnerAngle;
+		/// <summary>
+		/// Внешний угол для Type=Spot
+		/// </summary>
+		public Angle OuterAngle;
+
+		public enum MyLightType
+		{
+			Directional,
+			Ambient,
+			Point,
+			Spot
+		}
+	}
+}
