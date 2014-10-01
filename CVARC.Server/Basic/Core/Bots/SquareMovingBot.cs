b4efcd28faf9a555e84e29232c2206914ce05e1c
okulovsky <yuri.okulovsky@gmail.com>
2014-10-01 14:33:53 +0600

    +SquareMovingBot.cs, +TestProject

---
+++ CVARC.Server/Basic/Core/Bots/SquareMovingBot.cs
@@ -0,0 +1,35 @@
+﻿using AIRLab.Mathematics;
+using CVARC.Basic.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CVARC.Basic
+{
+    public class SquareMovingBot : Bot
+    {
+        int state = 0;
+        public override Controllers.Command MakeTurn()
+        {
+            state = (state + 1) % 8;
+            if (state % 2 == 0)
+                return new Command { RobotId=base.ControlledRobot, AngularVelocity = Angle.Pi / 2, Time = 1 };
+            else
+                return new Command { RobotId = base.ControlledRobot, LinearVelocity = 100, Time = 1 };
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TestProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using CVARC.Basic;
+using CVARC.Network;
+using RepairTheStarship;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var Competitions = new Gems.Levels.Level1();
+            var bundle = new CompetitionsBundle(Competitions, new GemsRules());
+
+            Competitions.HelloPackage = new HelloPackage { MapSeed = -1 };
+            Competitions.Initialize(
+                new CVARCEngine(bundle.Rules),
+                new[] { new RobotSettings(0, true), new RobotSettings(1, true) });
+
+            var bots = new[] { new SquareMovingBot(), new SquareMovingBot()};
+            var form = new TutorialForm(bundle.competitions);
+
+            new Thread(() => bundle.competitions.ProcessParticipants(true, int.MaxValue, bots)) 
+                { IsBackground = true }
+                .Start();
+            Application.Run(form);
+        }
+    }
+}
