4fe1eb5b8d31563d73e7976edf3a1b9129160d20
DmitryZyr <zyrianovdv@gmail.com>
2014-10-07 00:42:02 +0600

    рефакторинг

--- CVARC.Client/Level1ClientPython/level1Client.py
+++ CVARC.Client/Level1ClientPython/level1Client.py
@@ -1,58 +1,34 @@
-import os
-import socket
-import json
-import sys
+import os,socket,json,sys
 
-
-# Bots = "None", "Vaermina", "Azura", "MolagBal", "Sanguine"
 Side = {"Left": 0, "Right": 1, "Random": 2}
 Action = {"None": 0, "Grip": 1, "Release": 2, "WaitForExit": 3}
 
-
-class MapItem:
-    def __init__(self, json_obj):
-        self.x = json_obj["X"]
-        self.y = json_obj["Y"]
-        self.tag = json_obj["Tag"]
-    
-    def __str__(self):
-        return "X: {}, Y: {}, Tag: {}.".format(self.x, self.y, self.tag)
-
-
 class Position:
-    def __init__(self, json_obj):
-        self.angle = json_obj["Angle"]
-        self.x = json_obj["X"]
-        self.y = json_obj["Y"]
-        self.robot_number = json_obj["RobotNumber"]
-        
+    def __init__(self, x, y, angle):
+        self.angle = angle
+        self.x = x
+        self.y = y
     def __str__(self):
-        return "RobotNumber: {}, X: {}, Y: {}, Angle: {}.".format(self.robot_number, 
-                                                                  self.x, 
-                                                                  self.y, 
-                                                                  self.angle)
+        return "X: {}, Y: {}, Angle: {}.".format(self.x, self.y, self.angle)
 
-
-class SensorData:
+class JsonSensorData:
     def __init__(self, json_obj):
-        try:
-            self.robot_id = json_obj["RobotId"]["Id"]
-            self.positions = []
-            self.map_items = []
-            position = json_obj["Position"]["PositionsData"]
-            for i in range(len(position)):
-                self.positions.append(Position(position[i]))
-            
-            items = json_obj["MapSensor"]["MapItems"]
-            for i in range(len(items)):
-                self.map_items.append(MapItem(items[i]))
-        except KeyError:
-            print("Incorrect sensor data")
+        self.json_obj = json_obj;
 
+    def get_robot_id(self):
+        return self.json_obj["RobotId"]["Id"]
+
+    def has_gripped_detail(self):
+        return self.json_obj["DetailsInfo"]["HasGrippedDetail"]
+
+    def get_positions(self):
+        positions = [];
+        for i in self.json_obj["Position"]["PositionsData"]:
+            positions.append(Position(i["X"], i["Y"], i["Angle"]))
+        return positions
 
 def get_hello_package():
     return {"LevelName": "Level1", "Opponent": "None", "Side": Side["Left"]}
-
 
 def get_command(time, linear_velocity, angular_velocity_grad, action):
     return {"Action": action,
@@ -60,25 +36,23 @@ def get_command(time, linear_velocity, angular_velocity_grad, action):
             "LinearVelocity": linear_velocity,
             "Time": time}
 
-
 def read_ans(server):
     return json.loads(server.makefile('r').readline())
 
-
 def sendUtf8(server, string):
-	server.send(string.encode('utf-8'))
-	
+    server.send(string.encode('utf-8'))
+
 def send(server, json_obj):
-	sendUtf8(server, json.dumps(json_obj))
-	sendUtf8(server, '\n')
-	return read_ans(server)
+    sendUtf8(server, json.dumps(json_obj))
+    sendUtf8(server, '\n')
+    return read_ans(server)
 
 def send_hello_package(server):
     sendUtf8(server, json.dumps(get_hello_package()))
     sendUtf8(server, '\n')
     real_side = server.makefile('r').readline()
+    print "RealSide: ", real_side
     return read_ans(server)
-
     
 def run_server():
     if 'noRunServer' not in sys.argv:
@@ -88,29 +62,19 @@ def run_server():
     server.connect(('localhost', 14000))
     return server
 
-
-def print_sens_data(data):
-    print("X: {}, Y: {}".format(data.positions[data.robot_id].x, data.positions[data.robot_id].y))
-
-
 def main():
     server = run_server()
-    sensor_data = SensorData(send_hello_package(server))
-    sensor_data = SensorData(send(server, get_command(1, 0, -90, Action["None"])))
-    print_sens_data(sensor_data)
-    sensor_data = SensorData(send(server, get_command(1, 50, 0, Action["None"])))
-    print_sens_data(sensor_data)
-    sensor_data = SensorData(send(server, get_command(1, 0, 0, Action["Grip"])))
-    print_sens_data(sensor_data)
-    sensor_data = SensorData(send(server, get_command(1, -50, 0, Action["None"])))
-    print_sens_data(sensor_data)
-    sensor_data = SensorData(send(server, get_command(1, 0, 90, Action["None"])))
-    print_sens_data(sensor_data)
-    sensor_data = SensorData(send(server, get_command(1, 0, 0, Action["Release"])))
-    print_sens_data(sensor_data)
-    sensor_data = SensorData(send(server, get_command(0, 0, 0, Action["WaitForExit"])))
-    print_sens_data(sensor_data)
+    sensor_data = JsonSensorData(send_hello_package(server))
+    print "Robot Id: ", sensor_data.get_robot_id()
+    print "My robot position: ", sensor_data.get_positions()[sensor_data.get_robot_id()]
+    print "Has gripped detail: ", sensor_data.has_gripped_detail()
+    sensor_data = JsonSensorData(send(server, get_command(1, 0, -90, Action["None"])))
+    sensor_data = JsonSensorData(send(server, get_command(1, 50, 0, Action["None"])))
+    sensor_data = JsonSensorData(send(server, get_command(1, 0, 0, Action["Grip"])))
+    sensor_data = JsonSensorData(send(server, get_command(1, -50, 0, Action["None"])))
+    sensor_data = JsonSensorData(send(server, get_command(1, 0, 90, Action["None"])))
+    sensor_data = JsonSensorData(send(server, get_command(1, 0, 0, Action["Release"])))
+    sensor_data = JsonSensorData(send(server, get_command(0, 0, 0, Action["WaitForExit"])))
 
-    
 if __name__ == '__main__':
     main()
