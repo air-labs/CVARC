aaad616d30b2c35bae9daa55f79b82ec24d40bec
okulovsky <yuri.okulovsky@gmail.com>
2014-12-04 18:45:05 +0500

    fix

--- CVARC.V2/Framework/CVARC.Core/Units/MoveAndGrip/MoveAndGripRules.cs
+++ CVARC.V2/Framework/CVARC.Core/Units/MoveAndGrip/MoveAndGripRules.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AIRLab.Mathematics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,5 +26,13 @@ namespace CVARC.V2.Units
 			get;
 			private set;
 		}
+
+		public MoveAndGripRules(double linearVelocityLimit, Angle angularVelocityLimit)
+		{
+			LinearVelocityLimit = linearVelocityLimit;
+			AngularVelocityLimit = angularVelocityLimit;
+		}
+
+		public MoveAndGripRules() : this(50, Angle.HalfPi) { }
 	}
 }
