02438b2f2619801a663f473f98c6c89bd987b5e6
okulovsky <yuri.okulovsky@gmail.com>
2015-03-10 21:57:57 +0500

    + fix in test

--- CVARC.V2/Demo/DemoCompetitions/UniversalDemo/Tests/InteractionTests.cs
+++ CVARC.V2/Demo/DemoCompetitions/UniversalDemo/Tests/InteractionTests.cs
@@ -14,7 +14,11 @@ namespace Demo
 
 			//для AlignmentRect пришлось увеличить delta на проверке угла поворота до 0,005
 			logic.Tests["Interaction_Rect_Alignment"] = new RectangularInteractionTestBase(
-				LocationTest((frame,asserter)=>asserter.IsEqual(Angle.HalfPi.Grad,frame.Angle.Grad,1),
+				LocationTest((frame,asserter)=>
+					{
+						asserter.IsEqual(Angle.HalfPi.Grad,frame.Angle.Grad,1);
+						asserter.IsEqual(22.36, frame.Y, 1e-3);
+					},
 				rules.Move(-10),
 				rules.Rotate(Angle.HalfPi / 2),
 				rules.Move(50)));
@@ -25,7 +29,7 @@ namespace Demo
 			   (frame, asserter) => asserter.True(frame.X < 100 && frame.X > 70),
 			   rules.Move(100))); //думаю что тест не проходит из-за физики, поэтому не баг а фича
 
-
+			//тут явно нужны схожие тесты для круглого робота, плюс еще какие-нибудь ситуации со взаимодействием робота и стены
 
 		}
 	}
