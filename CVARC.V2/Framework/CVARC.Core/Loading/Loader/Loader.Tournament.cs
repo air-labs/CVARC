6be6900aa83a81bdf8c4cc9dd5cd73d14a5cf26b
okulovsky <yuri.okulovsky@gmail.com>
2015-03-16 10:49:15 +0500

    Впилил рыбу TournamentMode, пока думаю, что дальше делать.

---
+++ CVARC.V2/Framework/CVARC.Core/Loading/Loader/Loader.Tournament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CVARC.V2
+{
+	partial class Loader
+	{
+		public IWorld CreateTournament(CommandLineData cmdLineData)
+		{
+
+			return null;
+
+		}
+	}
+}
