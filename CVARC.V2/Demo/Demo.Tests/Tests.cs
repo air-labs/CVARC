17c1cb799b9cf8844bb9da211e2b5d32fb18fd19
okulovsky <yuri.okulovsky@gmail.com>
2015-03-17 21:34:52 +0500

    test file is regenerated

--- CVARC.V2/Demo/Demo.Tests/Tests.cs
+++ CVARC.V2/Demo/Demo.Tests/Tests.cs
@@ -1,6 +1,4 @@
 using NUnit.Framework;
-
-#region MovementTests
 namespace Movement { 
 [TestFixture] public partial class Round {
 [Test] public void Forward() { TestRunner.Run("Movement_Round_Forward"); }}}
@@ -30,78 +28,55 @@ namespace Movement {
 [Test] public void Linear() { TestRunner.Run("Movement_Limit_Linear"); }}}
 namespace Movement { 
 [TestFixture] public partial class Limit {
-[Test] public void Round() { TestRunner.Run("Movement_Limit_Round"); }}}
-namespace Movement {
-[TestFixture]
-public partial class Limit {
 [Test] public void Linear2() { TestRunner.Run("Movement_Limit_Linear2"); }}}
-namespace Movement {
-[TestFixture]
-public partial class Limit {
+namespace Movement { 
+[TestFixture] public partial class Limit {
+[Test] public void Round() { TestRunner.Run("Movement_Limit_Round"); }}}
+namespace Movement { 
+[TestFixture] public partial class Limit {
 [Test] public void Round2() { TestRunner.Run("Movement_Limit_Round2"); }}}
-#endregion
-
-#region InteractionTests
 namespace Interaction { 
 [TestFixture] public partial class Rect {
 [Test] public void Alignment() { TestRunner.Run("Interaction_Rect_Alignment"); }}}
 namespace Interaction { 
-[TestFixture] public partial class Rect {
-[Test] public void Collision() { TestRunner.Run("Interaction_Rect_Collision"); }}}
-namespace Interaction { 
 [TestFixture] public partial class Round {
 [Test] public void Alignment() { TestRunner.Run("Interaction_Round_Alignment"); }}}
-namespace Interaction {
-[TestFixture]
-public partial class Round {
+namespace Interaction { 
+[TestFixture] public partial class Round {
 [Test] public void Alignment2() { TestRunner.Run("Interaction_Round_Alignment2"); }}}
-#endregion
-
-#region GrippingTests
-namespace Gripping {
+namespace Interaction { 
+[TestFixture] public partial class Rect {
+[Test] public void Collision() { TestRunner.Run("Interaction_Rect_Collision"); }}}
+namespace Gripping { 
 [TestFixture] public partial class Rect {
 [Test] public void Grip() { TestRunner.Run("Gripping_Rect_Grip"); }}}
-namespace Gripping {
+namespace Gripping { 
 [TestFixture] public partial class Rect {
 [Test] public void GripThroughWall() { TestRunner.Run("Gripping_Rect_GripThroughWall"); }}}
-namespace Gripping {
+namespace Gripping { 
 [TestFixture] public partial class Rect {
 [Test] public void Release() { TestRunner.Run("Gripping_Rect_Release"); }}}
-namespace Gripping {
+namespace Gripping { 
 [TestFixture] public partial class Rect {
 [Test] public void GripUnGripable() { TestRunner.Run("Gripping_Rect_GripUnGripable"); }}}
-namespace Gripping {
-[TestFixture]
-public partial class Rect {
-[Test] public void BackGripping() { TestRunner.Run("Gripping_Rect_GripFromBack"); }}}
-#endregion
-
-#region Collision
-namespace Collision {
-[TestFixture]
-public partial class Rect {
+namespace Gripping { 
+[TestFixture] public partial class Rect {
+[Test] public void GripFromBack() { TestRunner.Run("Gripping_Rect_GripFromBack"); }}}
+namespace Collision { 
+[TestFixture] public partial class Rect {
 [Test] public void NoCollision() { TestRunner.Run("Collision_Rect_NoCollision"); }}}
-namespace Collision {
-[TestFixture]
-public partial class Rect {
+namespace Collision { 
+[TestFixture] public partial class Rect {
 [Test] public void CollisionCount() { TestRunner.Run("Collision_Rect_CollisionCount"); }}}
-namespace Collision {
-[TestFixture]
-public partial class Rect {
+namespace Collision { 
+[TestFixture] public partial class Rect {
 [Test] public void CollisionBox() { TestRunner.Run("Collision_Rect_CollisionBox"); }}}
-namespace Collision {
-[TestFixture]
-public partial class Rect {
+namespace Collision { 
+[TestFixture] public partial class Rect {
 [Test] public void NoBox() { TestRunner.Run("Collision_Rect_NoBox"); }}}
-namespace Collision {
-[TestFixture]
-public partial class Rect {
+namespace Collision { 
+[TestFixture] public partial class Rect {
 [Test] public void RotateNoBox() { TestRunner.Run("Collision_Rect_RotateNoBox"); }}}
-namespace Collision {
-[TestFixture]
-public partial class Rect {
+namespace Collision { 
+[TestFixture] public partial class Rect {
 [Test] public void RotateBox() { TestRunner.Run("Collision_Rect_RotateBox"); }}}
-#endregion
-
-#region CameraTests
-#endregion
