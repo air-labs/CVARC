a7eba1120acf671319521d1527115163d222c6ef
okulovsky <yuri.okulovsky@gmail.com>
2015-03-17 21:31:06 +0500

    + collisions are enabled in Demo

--- CVARC.V2/Demo/DemoCompetitions/UniversalDemo/DemoWorld.cs
+++ CVARC.V2/Demo/DemoCompetitions/UniversalDemo/DemoWorld.cs
@@ -10,7 +10,30 @@ namespace Demo
 {
     public class DemoWorld : World<DemoWorldState, IDemoWorldManager>
     {
+		public override void AdditionalInitialization()
+		{
+			base.AdditionalInitialization();
+			var detector = new CollisionDetector(this);
+			detector.FindControllableObject = side =>
+			{
+				var actor = Actors
+					.OfType<DemoRobot>()
+					.Where(z => z.ObjectId == side.ObjectId || z.Gripper.GrippedObjectId == side.ObjectId)
+					.FirstOrDefault();
+				if (actor != null)
+				{
+					side.ControlledObjectId = actor.ObjectId;
+					side.ControllerId = actor.ControllerId;
+				}
+			};
 
+			detector.Account = c =>
+			{
+				if (!c.Victim.IsControllable) return;
+				if (!detector.Guilty(c)) return;
+				Scores.Add(c.Offender.ControllerId, -1, "Collision");
+			};
+		}
 
         public override void CreateWorld()
         {
