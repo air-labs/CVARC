0434ba637bd53390dedc451489b322342649d4eb
okulovsky <yuri.okulovsky@gmail.com>
2015-03-26 00:00:11 +0500

    Добавил комменты

--- Client/Example/Program.cs
+++ Client/Example/Program.cs
@@ -11,24 +11,38 @@ namespace Client
 	{
 		private static readonly ClientSettings Settings = new ClientSettings
 		{
-			BotName = Bots.None,
-			Side = Side.Left,
-			LevelName = LevelName.Level1
+			Side = Side.Left, //Переключив это поле, можно отладить алгоритм для левой или правой стороны, а также для произвольной стороны, назначенной сервером
+			LevelName = LevelName.Level2, //Это не нужно трогать на данном этапе
+			MapNumber=-1 //Задавая различные значения этого поля, вы можете сгенерировать различные случайные карты
 		};
 
+		//Это не нужно менять
 		const string NetworkServerDirectory = ".\\..\\..\\..\\Binaries\\";
 
 		private static void Main(string[] args)
 		{
-			var server = new CvarcClient(args, Settings, NetworkServerDirectory).GetServer<BaseSensorData>();
+			var server = new CvarcClient(args, Settings, NetworkServerDirectory).GetServer<PositionSensorsData>();
 			var helloPackageAns = server.Run();
-			Console.WriteLine("Your Side: {0}", helloPackageAns.RealSide);
-			server.SendCommand(new Command { AngularVelocity = Angle.FromGrad(-90), Time = 1 });
-			server.SendCommand(new Command { LinearVelocity = 50, Time = 1 });
-			server.SendCommand(new Command { Action = CommandAction.Grip, Time = 1 });
-			server.SendCommand(new Command { LinearVelocity = -50, Time = 1 });
-			server.SendCommand(new Command { AngularVelocity = Angle.FromGrad(90), Time = 1 });
-			server.SendCommand(new Command { Action = CommandAction.Release, Time = 1 });
+
+			//Здесь вы можете узнать сторону, назначенную вам сервером в случае, если запросили Side.Random. 
+			//ВАЖНО!
+			//Side и MapNumber влияют на сервер только на этапе отладки. В боевом режиме и то, и другое будет назначено сервером
+			//вне зависимости от того, что вы указали в Settings! Поэтому ваш итоговый алгоритм должен использоать helloPackageAns.RealSide
+			Console.WriteLine("Your Side: {0}", helloPackageAns.RealSide); 
+
+			PositionSensorsData sensorsData = null;
+
+			//Так вы можете отправлять различные команды. По результатам выполнения каждой команды, вы получите sensorsData, 
+			//который содержит информацию о происходящем на поле
+			sensorsData = server.SendCommand(new Command { AngularVelocity = Angle.FromGrad(-90), Time = 1 });
+			sensorsData = server.SendCommand(new Command { LinearVelocity = 50, Time = 1 });
+			sensorsData = server.SendCommand(new Command { Action = CommandAction.Grip, Time = 1 });
+			sensorsData = server.SendCommand(new Command { LinearVelocity = -50, Time = 1 });
+			sensorsData = server.SendCommand(new Command { AngularVelocity = Angle.FromGrad(90), Time = 1 });
+			sensorsData = server.SendCommand(new Command { Action = CommandAction.Release, Time = 1 });
+
+			//Используйте эту команду в конце кода для того, чтобы в режиме отладки все окна быстро закрылись, когда вы откатали алгоритм.
+			//Если вы забудете это сделать, сервер какое-то время будет ожидать команд от вашего отвалившегося клиента. 
 			server.Exit();
 		}
 	}
