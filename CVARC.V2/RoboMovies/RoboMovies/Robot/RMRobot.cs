1f1ea90480bac33fb0cc353cfafd88ab91e0288b
Ivan Poroshin <niemandkun@yandex.ru>
2015-07-20 15:15:02 +0600

    Логика подсчета очков за башни.

--- CVARC.V2/RoboMovies/RoboMovies/Robot/RMRobot.cs
+++ CVARC.V2/RoboMovies/RoboMovies/Robot/RMRobot.cs
@@ -22,7 +22,7 @@ namespace RoboMovies
                     Func<string, bool> isAttachedToStand = s =>
                         (s = World.Engine.FindParent(s)) != null &&
                         World.IdGenerator.KeyOfType<RMObject>(s) &&
-                        World.IdGenerator.GetKey<RMObject>(s).Type == ObjectType.Stand;
+                        GetObjectFromId(s).Type == ObjectType.Stand;
 
                     var allCollectable = World.IdGenerator.GetAllPairsOfType<RMObject>()
                 		.Where(z => z.Item1.Type == ObjectType.Stand || z.Item1.Type == ObjectType.Light)
@@ -39,6 +39,12 @@ namespace RoboMovies
 			
             base.TowerBuilder.GrippingPoint = new Frame3D(15, 0, 5);
             base.TowerBuilder.OnRelease = CheckTowerPosition;
+            base.TowerBuilder.OnGrip = (id, location) =>
+                {
+                    var obj = GetObjectFromId(id);
+                    if (obj.Color != robotColor && obj.Color != SideColor.Any)
+                        World.Scores.Add(ControllerId, -10, "Took the stand of invalid color.");
+                };
 
             robotColor = ControllerId == TwoPlayersId.Left ? SideColor.Yellow : SideColor.Green;
 		}
@@ -50,10 +56,23 @@ namespace RoboMovies
 
         void CheckTowerPosition(HashSet<string> tower, Frame3D location)
         {
+            var bonus = TowerBuilder.ContainsBall ? 3 : 0;
+
+            var validTower = tower
+                .Select(x => GetObjectFromId(x))
+                .Where(x => x.Color == robotColor && x.Type == ObjectType.Stand);
+
             if (World.IsInsideStartingArea(location, robotColor) || World.IsInsideBuildingArea(location))
-                foreach (var item in tower)
-                    World.Scores.Add(ControllerId, 2, 
+                foreach (var item in validTower)
+                    World.Scores.Add(ControllerId, 2 + bonus, 
                         String.Format("{0} stand(s) has been deployed in correct place.", tower.Count));
+        }
+
+        RMObject GetObjectFromId(string id)
+        {
+            if (!World.IdGenerator.KeyOfType<RMObject>(id))
+                throw new ArgumentException("This id is not bind to any RMObject.");
+            return World.IdGenerator.GetKey<RMObject>(id);
         }
     }
 }
