7de1e1320ef8d7364e40e6ea071cd7661531648c
Ivan Poroshin <niemandkun@yandex.ru>
2015-07-22 17:46:26 +0600

    Написал тесты для TowerBuilder'а

--- CVARC.V2/RoboMovies/RoboMovies/Tests/TowerBuildingTests.cs
+++ CVARC.V2/RoboMovies/RoboMovies/Tests/TowerBuildingTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CVARC.V2;
+using AIRLab.Mathematics;
 
 namespace RoboMovies
 {
@@ -11,16 +12,120 @@ namespace RoboMovies
         [TestLoader]
         public void LoadTowerBuilderTests(LogicPart logic, MoveAndBuildRules rules)
         {
-            AddTest(logic, "SimpleTest", TowerBuilderTest(
-                0,
-                rules.Move(100),
-                rules.Stand(1)
+            AddTest(logic, "Grip_Single_Detail", TowerBuilderTest(
+                1,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect()
             ));
 
-            AddTest(logic, "SimpleScoreTest", ScoreTest(
+            AddTest(logic, "Grip_Multiple_Details", TowerBuilderTest(
+                2,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(15),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(10),
+                rules.Stand(1),
+                rules.Collect()
+            ));
+
+            AddTest(logic, "Grip_Light_Before_Stand", TowerBuilderTest(
+                1,
+                rules.Rotate(Angle.Pi),
+                rules.Move(10),
+                rules.Stand(0.1),
+                rules.Collect()
+            ));
+
+            AddTest(logic, "Grip_Light_After_Stand", TowerBuilderTest(
+                1,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Rotate(Angle.Pi),
+                rules.Move(25),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(75),
+                rules.Stand(0.1),
+                rules.Collect()
+            ));
+            
+            AddTest(logic, "Release_Single_Detail", TowerBuilderTest(
+                0,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.BuildTower()
+            ));
+
+            AddTest(logic, "Release_Multiple_Detail", TowerBuilderTest(
+                0,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(15),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(10),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.BuildTower()
+            ));
+
+            AddTest(logic, "Release_Through_Border", TowerBuilderTest(
+                1,
+                rules.Move(100),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(100),
+                rules.Stand(0.1),
+                rules.BuildTower(),
+                rules.Move(-50),
+                rules.Stand(0.1)
+            ));
+
+            AddTest(logic, "Grip_Through_Border", TowerBuilderTest(
                 0,
                 rules.Move(100),
-                rules.Stand(1)
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(100),
+                rules.Stand(0.1),
+                rules.BuildTower(),
+                rules.Collect(),
+                rules.Move(-50),
+                rules.Stand(0.1)
+            ));
+
+            AddTest(logic, "Grip_Tower", TowerBuilderTest(
+                2,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(15),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(10),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.BuildTower(),
+                rules.Collect()
             ));
         }
     }
---
+++ CVARC.V2/RoboMovies/RoboMovies/Tests/TowerScoringTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CVARC.V2;
+using AIRLab.Mathematics;
+
+namespace RoboMovies
+{
+    partial class RMLogicPartHelper
+    {
+        [TestLoader]
+        public void LoadTowerScoringTests(LogicPart logic, MoveAndBuildRules rules)
+        {
+            AddTest(logic, "Scores_Zero", ScoreTest(
+                0,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Bottom_Building_Area", ScoreTest(
+                2,
+                rules.Move(100),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(50),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Starting_Area_Square", ScoreTest(
+                2,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(-25),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(65),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Starting_Area_Circle", ScoreTest(
+                2,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(-25),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(35),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Building_In_Yellow_Square", ScoreTest(
+                0,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(20),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(170),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+
+            AddTest(logic, "Scores_Big_Tower", ScoreTest(
+                2 * 2,
+                rules.Move(65),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(15),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(10),
+                rules.Stand(1),
+                rules.Collect(),
+                rules.Move(20),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(30),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Tower_With_Light", ScoreTest(
+                2 + 3,
+                rules.Rotate(Angle.Pi),
+                rules.Move(10),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Rotate(Angle.Pi),
+                rules.Move(105),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(50),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+            
+            /* FIXME: не хватает времени на тест! Как увеличить время? о_О */
+            AddTest(logic, "Scores_Big_Tower_With_Light", ScoreTest(
+                (2 + 3) * 2,
+                rules.Rotate(Angle.Pi),
+                rules.Move(10),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Rotate(Angle.Pi),
+                rules.Move(75),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(15),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(10),
+                rules.Stand(1),
+                rules.Collect(),
+                rules.Move(20),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(30),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+
+            AddTest(logic, "Scores_Light_Without_Stands", ScoreTest(
+                0,
+                rules.Rotate(Angle.Pi),
+                rules.Move(10),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(-10),
+                rules.Stand(0.1),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Scam_Test", ScoreTest(
+                2,
+                rules.Move(100),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect(),
+                rules.Move(50),
+                rules.Stand(0.1),
+                rules.BuildTower(),
+                rules.Collect(),
+                rules.BuildTower(),
+                rules.Collect(),
+                rules.BuildTower(),
+                rules.Collect(),
+                rules.BuildTower()
+            ));
+            
+            AddTest(logic, "Scores_Invalid_Stand_Penalty", ScoreTest(
+                -10,
+                rules.Move(135),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(25),
+                rules.Stand(0.1),
+                rules.Collect()
+            ));
+        }
+    }
+}
