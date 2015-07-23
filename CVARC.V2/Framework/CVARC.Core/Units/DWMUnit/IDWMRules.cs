2c1590143eb19d9462b2713c7bc8e84a62f165c8
NoCatsNoParty <tprdfg@gmail.com>
2015-07-23 05:42:26 +0500

    add speed limits

--- CVARC.V2/Framework/CVARC.Core/Units/DWMUnit/IDWMRules.cs
+++ CVARC.V2/Framework/CVARC.Core/Units/DWMUnit/IDWMRules.cs
@@ -11,6 +11,8 @@ namespace CVARC.V2
 		double WheelRadius { get; set; }
 		double DistanceBetweenWheels { get; set; }
 		Angle RotationSpeedLimit { get; set; }
+        double LinearVelocityLimit { get; set; }
+        Angle AngularVelocityLimit { get; set; } 
 	}
 
 	public interface IDWMRules<TCommand> : IDWMRules
