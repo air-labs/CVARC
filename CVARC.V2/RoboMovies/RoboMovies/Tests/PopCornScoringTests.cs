7aa2c83a17b5a99403b3b7e32b55932d60137724
Ivan Poroshin <niemandkun@yandex.ru>
2015-07-29 13:32:47 +0600

    Тесты для попкорна

---
+++ CVARC.V2/RoboMovies/RoboMovies/Tests/PopCornScoringTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CVARC.V2;
+using AIRLab.Mathematics;
+
+namespace RoboMovies
+{
+    partial class RMLogicPartHelper
+    {
+        [TestLoaderMethod]
+        public void LoadPopCornScoringTests(LogicPart logic, RMRules rules)
+        {
+            AddTest(logic, "Scores_PopCornStartinArea", ScoreTest(
+                4,
+                rules.Move(64),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(-64),
+                rules.Stand(0.1),
+                rules.Release()
+            ));
+            
+            AddTest(logic, "Scores_PopCornUpperCinema", ScoreTest(
+                4,
+                rules.Move(64),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(130),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(40),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(60),
+                rules.Stand(0.1),
+                rules.Release()
+            ));
+            
+            AddTest(logic, "Scores_PopCornBottomCinema", ScoreTest(
+                4,
+                rules.Move(64),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(130),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(40),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(60),
+                rules.Stand(0.1),
+                rules.Release()
+            ));
+            
+            AddTest(logic, "Scores_PopCornBuildingArea", ScoreTest(
+                0,
+                rules.Move(115),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(-60),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Move(-15),
+                rules.Stand(0.1),
+                rules.Release(),
+                rules.Move(40),
+                rules.Stand(0.1)
+            ));
+
+            AddTest(logic, "Scores_TwoPopCornOneLocation", ScoreTest(
+                4,
+                rules.Move(115),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(-60),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Move(60),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(-115),
+                rules.Stand(0.1),
+                rules.Release(),
+                rules.Move(64),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(-40),
+                rules.Stand(0.1),
+                rules.Release()
+            ));
+            
+            AddTest(logic, "Scores_TwoPopCornTwoLocations", ScoreTest(
+                4 + 4,
+                rules.Move(115),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(-60),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Move(60),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(-115),
+                rules.Stand(0.1),
+                rules.Release(),
+                rules.Move(64),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(130),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Move(40),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(60),
+                rules.Stand(0.1),
+                rules.Release()
+            ));
+
+            AddTest(logic, "Scores_GrippedPopCornShouldBeZero", ScoreTest(
+                0,
+                rules.Move(64),
+                rules.Rotate(-Angle.HalfPi),
+                rules.Stand(0.1),
+                rules.Grip(),
+                rules.Rotate(Angle.HalfPi),
+                rules.Move(-64),
+                rules.Stand(0.1)
+            ));
+        }
+    }
+}
