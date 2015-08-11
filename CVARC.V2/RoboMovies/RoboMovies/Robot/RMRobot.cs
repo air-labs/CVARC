170e26d3ece2eb17504b2635078ea0771078acac
Ivan Poroshin <niemandkun@yandex.ru>
2015-08-11 17:11:00 +0500

    Немного передвинул GrippingPoints у юнитов, которые могут аттачить, чтобы избежать проблем с застреванием в текстурах в юнити.

--- CVARC.V2/RoboMovies/RoboMovies/Robot/RMRobot.cs
+++ CVARC.V2/RoboMovies/RoboMovies/Robot/RMRobot.cs
@@ -32,6 +32,9 @@ namespace RoboMovies
 		public override void AdditionalInitialization()
 		{
             base.AdditionalInitialization();
+
+            Debugger.Log(RMDebugMessage.Logic, "RMRobot initialization started...");
+
             SimpleMovementUnit = new SimpleMovementUnit(this);
             TowerBuilder = new TowerBuilderUnit(this);
             Gripper = new GripperUnit(this);
@@ -40,6 +43,7 @@ namespace RoboMovies
             var robotColor = ControllerId == TwoPlayersId.Left ? SideColor.Yellow : SideColor.Green;
 
             #region TowerBuilder
+            Debugger.Log(RMDebugMessage.Logic, "    Initializing tower builder");
             TowerBuilder.FindCollectable = () =>
                 {
                     Func<string, bool> isAttachedToStand = s =>
@@ -47,23 +51,36 @@ namespace RoboMovies
                         World.IdGenerator.KeyOfType<RMObject>(s) &&
                         World.GetObjectById(s).Type == ObjectType.Stand;
 
-                    var allCollectable = World.IdGenerator.GetAllPairsOfType<RMObject>()
-                		.Where(z => z.Item1.Type == ObjectType.Stand || z.Item1.Type == ObjectType.Light)
-                		.Where(z => World.Engine.ContainBody(z.Item2))
+                    Debugger.Log(RMDebugMessage.Logic, "Searching for stands...");
+                    var allStands = World.IdGenerator.GetAllPairsOfType<RMObject>()
+                        .Where(z => z.Item1.Type == ObjectType.Stand || z.Item1.Type == ObjectType.Light)
+                        .Where(z => World.Engine.ContainBody(z.Item2))
+                        .ToArray();
+
+                    foreach (var x in allStands)
+                        Debugger.Log(RMDebugMessage.Logic, "    Stand found: " + x.Item2);
+                    
+                    Debugger.Log(RMDebugMessage.Logic, "Searching for collectable stands...");
+                    var allCollectable = allStands
                 		.Where(z => isAttachedToStand(z.Item2) || !World.Engine.IsAttached(z.Item2)) 
                         .OrderByDescending(z => World.Engine.GetAbsoluteLocation(z.Item2).Z)
                 		.Select(z => new { Id = z.Item2, Availability = TowerBuilder.GetAvailability(z.Item2) })
-                		.ToList();
+                		.ToArray();
+
+                    foreach (var x in allCollectable)
+                        Debugger.Log(RMDebugMessage.Logic, "    Collectable stand found: " + x.Id);
 
                     return allCollectable
                         .Where(z => z.Availability.Distance < 10)
                         .Select(z => z.Id);
                 };
             
-            TowerBuilder.GrippingPoint = new Frame3D(15, 0, 5);
+            TowerBuilder.GrippingPoint = new Frame3D(16, 0, 5);
             
             TowerBuilder.OnGrip = (id, location) =>
                 {
+                    Debugger.Log(RMDebugMessage.Logic, String.Format("Gripping object {0} at {1}", id, location));
+
                     var obj = World.GetObjectById(id);
                     if (obj.Color != robotColor && obj.Color != SideColor.Any)
                         World.Scores.Add(ControllerId, -10, "Took the stand of invalid color.");
@@ -75,6 +92,9 @@ namespace RoboMovies
 
             TowerBuilder.OnRelease = (ids, location) => 
             {
+                foreach(var id in ids)
+                    Debugger.Log(RMDebugMessage.Logic, String.Format("Releasing object {0} at {1}", id, location));
+
                 var light = ids
                     .Where(id => World.IdGenerator.GetKey<RMObject>(id).Type == ObjectType.Light)
                     .FirstOrDefault();
@@ -84,15 +104,21 @@ namespace RoboMovies
             #endregion
 
             #region Gripper
+            Debugger.Log(RMDebugMessage.Logic, "    Initializing gripper");
             Gripper.FindDetail = () =>
                 {
+                    Debugger.Log(RMDebugMessage.Logic, "Searching for collectable popcorn...");
+                    
                     var allCollectable = World.IdGenerator.GetAllPairsOfType<RMObject>()
                 		.Where(z => z.Item1.Type == ObjectType.PopCorn)
                 		.Where(z => World.Engine.ContainBody(z.Item2))
                 		.Where(z => !World.Engine.IsAttached(z.Item2)) 
                         .OrderByDescending(z => World.Engine.GetAbsoluteLocation(z.Item2).Z)
                 		.Select(z => new { Id = z.Item2, Availability = Gripper.GetAvailability(z.Item2) })
-                		.ToList();
+                		.ToArray();
+
+                    foreach (var x in allCollectable)
+                        Debugger.Log(RMDebugMessage.Logic, "    Popcorn found: " + x.Id);
 
                     return allCollectable
                         .Where(z => z.Availability.Distance < 10)
@@ -101,14 +127,11 @@ namespace RoboMovies
                         .FirstOrDefault();
                 };
             
-            Gripper.GrippingPoint = new Frame3D(-15, 0, 5);
+            Gripper.GrippingPoint = new Frame3D(-17, 0, 5);
             Gripper.OnRelease = (id, location) => World.Engine.Detach(id, location.NewZ(3));
             #endregion
-        }
-        
-        double Distance(string from, string to)
-        {
-            return Geometry.Hypot(World.Engine.GetAbsoluteLocation(from) - World.Engine.GetAbsoluteLocation(to));
+
+            Debugger.Log(RMDebugMessage.Logic, "RMRobot initialization finished!");
         }
     }
 }
