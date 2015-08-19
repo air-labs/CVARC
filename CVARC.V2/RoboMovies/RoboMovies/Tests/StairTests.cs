a2dd85f682f764081ab6fa8a651b1eb348ee4412
Ivan Poroshin <niemandkun@yandex.ru>
2015-08-19 15:35:41 +0500

    Исправления в тестах, рефакторинг.

--- CVARC.V2/RoboMovies/RoboMovies/Tests/StairTests.cs
+++ CVARC.V2/RoboMovies/RoboMovies/Tests/StairTests.cs
@@ -20,24 +20,20 @@ namespace RoboMovies
                 rules.UpLadder(),
             };
 
-            var greenUp = new RMCommand[] {
-                rules.Move(140),
-                rules.Rotate(Angle.HalfPi),
-                rules.Move(25),
-                rules.Stand(0.1),
-                rules.UpLadder(),
-            };
+            var greenUp = new RMCommand[yellowUp.Length];
+            yellowUp.CopyTo(greenUp, 0);
+            greenUp[0] = rules.Move(140);
 
             AddTest(logic, "Stairs_YellowSuccess", PositionTest(-25, 80, 10, yellowUp));
             AddTest(logic, "Stairs_GreenFailure", PositionTest(25, 30, 10, greenUp));
             
-            AddTest(logic, "Stairs_Scoring_Success", ScoreTest(50, yellowUp));
+            AddTest(logic, "Stairs_Scoring_Success", ScoreTest(15, yellowUp));
             AddTest(logic, "Stairs_Scoring_Failure", ScoreTest(0, greenUp));
             
             AddTest(logic, "Stairs_Scoring_UpWithoutCommand", ScoreTest(0, 
                 rules.Move(90),
                 rules.Rotate(Angle.HalfPi),
-                rules.Move(50),
+                rules.Move(80),
                 rules.Stand(0.1)
             ));
         }
