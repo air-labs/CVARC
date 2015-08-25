1c68e48b8f8aa897d87bca292b379d01a79bf0f4
Ivan Poroshin <niemandkun@yandex.ru>
2015-08-25 18:24:07 +0500

    Сенсор попкорна.

---
+++ CVARC.V2/RoboMovies/RoboMovies/Sensors/PopCornSensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CVARC.V2;
+
+namespace RoboMovies
+{
+    public class PopCornSensor : Sensor<int, IRMRobot>
+    {
+        public override int Measure()
+        {
+            if (Actor.Gripper.GrippedObjectId != null)
+                return (Actor.World as RMWorld).PopCornFullness[Actor.Gripper.GrippedObjectId];
+            return 0;
+        }
+    }
+}
